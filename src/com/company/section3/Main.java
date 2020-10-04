36b6373ff6182fbef0c2a7ca2d99029925de9320
Sergei <sergei1733@yandex.ru>
2020-10-04 19:20:11 +0500

    1

---
+++ src/com/company/section3/Main.java
@@ -0,0 +1,35 @@
+package com.company.section3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+5) Напишите методы union(Set<?>... set) и intersect(Set<?> ... set), реализующих операции объединения и
+   пересечения множеств. Протестируйте работу этих методов на предварительно заполненных множествах.
+ */
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Set<Integer> union1 = new TreeSet<>();
+        Set<Integer> union2 = new TreeSet<>();
+        for (int i = 0; i < 5; i++) {
+            union1.add(in.nextInt());
+        }
+        for (int i = 0; i < 5; i++) {
+            union2.add(in.nextInt());
+        }
+        System.out.println(union(union1,union2));
+
+    }
+
+    public static Set union(Set<Integer> union1, Set<Integer> union2){
+        union1.addAll(union2);
+        return union1;
+
+    }
+
+
+}
