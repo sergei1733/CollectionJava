6ccc6bdc2c42a8628d6790cafc6608d4f9cbef01
Sergei <sergei1733@yandex.ru>
2020-10-04 18:40:15 +0500

    1

--- src/com/company/section2/Main.java
+++ src/com/company/section2/Main.java
@@ -1,8 +1,17 @@
 package com.company.section2;
 
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class Main {
 
+    public static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code here
+	String s;
+        Set<String> set = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            set.add(s= in.nextLine());
+        }
+        System.out.println(set);
     }
 }
